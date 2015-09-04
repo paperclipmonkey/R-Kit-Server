14348dd8950be554146878b8a5c9ca475882d568
Michael Waterworth <mike@3equals.co.uk>
2015-09-04 16:47:59 +0100

    Externalise S3 url

--- s3-client.js
+++ s3-client.js
@@ -1,5 +1,6 @@
 module.exports = (function (app) {
   var s3 = require('s3')
+  var CONFIG = require('config')
 
   var client = s3.createClient({
     maxAsyncS3: 20, // this is the default
@@ -8,9 +9,9 @@ module.exports = (function (app) {
     // multipartUploadThreshold: 20971520, // this is the default (20 MB)
     // multipartUploadSize: 15728640, // this is the default (15 MB)
     s3Options: {
-      accessKeyId: 'AKIAIQCOD2VSJIYGIUAA',
-      secretAccessKey: '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',
-      region: 'eu-west-1'
+      accessKeyId: process.env.S3_ACCESS_KEY_ID || CONFIG['S3_ACCESS_KEY_ID'],
+      secretAccessKey: process.env.S3_SECRET_ACCESS_KEY || CONFIG['S3_SECRET_ACCESS_KEY'],
+      region: process.env.S3_REGION || CONFIG['S3_REGION']
     // any other options are passed to new AWS.S3()
     // See: http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/Config.html#constructor-property
     }
